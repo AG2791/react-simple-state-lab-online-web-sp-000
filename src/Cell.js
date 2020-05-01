c2be270c7a1c27e3f7e39650e361e1896d5a8632
AJ Guiles <ashiguiles@gmail.com>
2020-05-01 04:58:50 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -17,15 +17,15 @@ export default class Cell extends Component {
     })
   }
   
-  render() {
-    return (
-      <div 
-        className="cell"
-        style={{backgroundColor: this.state.color}}
-        onClick={this.changeColor}>
-      </div>
-    )
-  }
+  // render() {
+  //   return (
+  //     <div 
+  //       className="cell"
+  //       style={{backgroundColor: this.state.color}}
+  //       onClick={this.changeColor}>
+  //     </div>
+  //   )
+  // }
   
    
 } 
